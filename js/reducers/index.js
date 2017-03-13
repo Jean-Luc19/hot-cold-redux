6f8cf32c8788de8522a313aa23752b23e5c7e2d6
Aaron Gottlieb <aticus19@gmail.com>
2017-03-12 18:22:30 -0600

    adds gameReducer.js

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,8 +1,43 @@
 import * as actions from '../actions/index';
 
 const initialRepositoryState = {
-    target: 0,
+    target: Math.floor(Math.random() * 100),
+    match: false,
+    temps: ['frigid', 'nippy', 'tepid', 'smoldering', 'scortching', 'en-freaking-fuego' ],
+    currentTemp: '',
     guesses: [],
 }
 
-export const
+export const gameReducer = (state=initialRepositoryState, action) => {
+    if (action.type === actions.NEW_GAME) {
+        return initialRepositoryState
+    }
+    else if (action.type === actions.GUESS){
+        const newGuessArray = [...state.guesses, action.guessNumber];
+
+        if (action.guessNumber === state.target) {
+            return Object.assign({}, state, {match: true})
+        }
+        else if (Math.abs(action.guessNumber - state.target) <= 5) {
+            return Object.assign({}, state, {guesses: newGuessArray}, {currentTemp: state.temps[5]})
+        }
+        else if (Math.abs(action.guessNumber - state.target) <= 10) {
+            return Object.assign({}, state, {guesses: newGuessArray}, {currentTemp: state.temps[4]})
+        }
+        else if (Math.abs(action.guessNumber - state.target) <= 25) {
+            return Object.assign({}, state, {guesses: newGuessArray}, {currentTemp: state.temps[3]})
+        }
+        else if (Math.abs(action.guessNumber - state.target) <= 40) {
+            return Object.assign({}, state, {guesses: newGuessArray}, {currentTemp: state.temps[2]})
+        }
+        else if (Math.abs(action.guessNumber - state.target) <= 60) {
+            return Object.assign({}, state, {guesses: newGuessArray}, {currentTemp: state.temps[1]})
+        }
+        else if (Math.abs(action.guessNumber - state.target) > 60) {
+            return Object.assign({}, state, {guesses: newGuessArray}, {currentTemp: state.temps[0]})
+        }
+    }
+    return state;
+
+
+};
